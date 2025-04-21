ef025fc0e26aa32bf920aeaac1c88afb457af67e
varshath04 <145836423+varshath04@users.noreply.github.com>
2025-04-21 15:08:59 +0530

    Update app.py

--- updated_votingap/app.py
+++ updated_votingap/app.py
@@ -152,4 +152,5 @@ def vote(poll_id):
     return render_template('vote.html', poll=poll)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
